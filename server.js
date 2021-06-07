326dea46f5dc8ff353d9d80346ae195b65f04a26
Héctor Realpe <hrealpe@gmail.com>
2021-06-07 09:10:25 +0200

    prueba

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname, 'rellenar'));
+app.use(express.static(__dirname, 'dist/testAngular'));
 app.get('/*', function (req, res) {
-    res.sendFile("index.html", { root: __dirname + 'rellenar'});
+    res.sendFile("index.html", { root: __dirname + 'dist/testAngular/'});
 });
 
 app.listen(process.env.PORT || 8080);
